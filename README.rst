109373943cd5f68219b0b81c5e656b3d4171b24d
Luke Parry <dev@lukeparry.uk>
2020-04-06 17:05:07 +0100

    Initial Commit for Python OpenCL Helper
    Signed-off-by: Luke Parry <dev@lukeparry.uk>

--- README.rst
+++ README.rst
@@ -1,7 +1,9 @@
 PyOCL Module Repository
 ========================
 
-This project provides some helper classes to work
+Provides convenience methods and boiler-plate code for interacting and setting up the OpenCL compute environment based on the capabilities provided
+by PyOpenCL. This is to aid setting up simulations for use in development. Common routines and flags are enabled for convenience to improve productivity and provide a consistent
+environment to develop in.
 
 `Learn more <http://lukeparry.uk/>`_.
 
--- pyocl/core.py
+++ pyocl/core.py
@@ -1,30 +1,18 @@
 # -*- coding: utf-8 -*-
 
-
 from enum import Enum, auto
 from typing import List
 import logging
-import numpy
-import time
 import pyopencl as cl
 
-from . import helpers
-
-
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
-
-
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
-
-
 class Core:
-    """
 
+    """
+    PyOCL Core Class
+
+    Provides methods for interacting and setting up the OpenCL compute environment based on the capabilities provided
+    by PyOpenCL. Common routines and flags are enabled for convenience to improve productivity and provide a consistent
+    environment to develop in.
     """
 
     class OpenCLFlags(Enum):
@@ -32,7 +20,7 @@ class Core:
         Enums for setting options for the OpenCL Class
         """
         ENABLE_DEBUG = auto()
-        DISABLE_CACHE = auto()  # 'PYOPENCL_NO_CACHE=1'
+        DISABLE_CACHE = auto()            # 'PYOPENCL_NO_CACHE=1'
         DISABLE_NON_FINITE_MATH = auto()  # '-cl-finite-math-only, -cl-finite-math-only'
 
     def __init__(self, useGPU: bool = True) -> None:
@@ -83,11 +71,17 @@ class Core:
                                        devices=[self.device])
 
     @property
-    def device(self):
+    def device(self) -> cl.Device:
+        """
+        Returns the current selected OpenCL Device
+        """
         return self._device
 
     @property
     def platform(self) -> cl.Platform:
+        """
+        Returns the current OpenCL Device
+        """
         return self._platform
 
     @property
@@ -101,6 +95,11 @@ class Core:
         return self._isUsingGPU
 
     def hasSharedMemory(self) -> bool:
+
+        """
+        Returns if the currently selected Compute Device has shared memory capability (e.g. APU, Intel Platforms)
+        :return: bool
+        """
         if self.deviceType() == 'Intel GPU':
             return True
         else:
@@ -113,29 +112,68 @@ class Core:
     ### Helper functions ###
     @property
     def gpuDevices(self) -> List[cl.Device]:
+
+        """
+        Returns list of GPU devices based on the currently chosen platform
+        :return: list of clDevices
+        """
         return self.platform.get_devices(cl.device_type.GPU)
 
     @property
     def cpuDevices(self) -> List[cl.Device]:
+
+        """
+        Returns list of CPU devices based on the currently chosen platform
+        :return: list of clDevices
+        """
         return self.platform.get_devices(cl.device_type.CPU)
 
     @property
     def computeUnits(self) -> int:
+
+        """
+        Returns the number of compute units available on the selected compute device
+        :return: number of compute units available
+        """
         return self.device.max_compute_units
 
     @property
     def localMemorySize(self) -> int:
+
+        """
+        Returns the local size of memory available on the compute device
+        :return: Size of local memory available [bytes]
+        """
         return self.device.local_mem_size
 
     @property
     def globalMemorySize(self) -> int:
+
+        """
+        Returns the global size of memory available on the compute device
+        :return: Size of global memory available [bytes]
+        """
         return self.device.global_mem_size
 
     def hasFloat16(self) -> bool:
+
+        """
+         Returns if the compute device has native float16 support
+         :return: number of compute units available
+         """
         return 'cl_khr_fp16' in self.platform.extensions
 
     def hasDouble(self) -> bool:
+
+        """
+         Returns if the compute device has native float64 (double) support
+         :return: number of compute units available
+         """
         return 'cl_khr_fp64' in self.platform.extensions
 
     def hasGLShare(self) -> bool:
+
+        """
+         Returns if the compute device has native GL Sharing Capabilities (within driver)
+         """
         return 'cl_khr_gl_sharing' in self.platform.extensions

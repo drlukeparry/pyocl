6fcc143ab808262e804769190ce45b34f3efd448
Luke Parry <dev@lukeparry.uk>
2020-04-20 15:23:49 +0100

    Added method to initialise with a specific device and included other additional helper methods. Further tests on Linux.
    Signed-off-by: Luke Parry <dev@lukeparry.uk>

--- pyocl/core.py
+++ pyocl/core.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 from enum import Enum, auto
-from typing import List
+from typing import List, Tuple
 import logging
 import pyopencl as cl
 
@@ -13,7 +13,7 @@ class OpenCLFlags(Enum):
     ENABLE_DEBUG = auto()
     ENABLE_COMPILER_OUTPUT = auto()
     ENABLE_CACHE = auto()  # 'PYOPENCL_NO_CACHE=1'
-    DISABLE_NON_FINITE_MATH = auto()  # '-cl-finite-math-only
+    DISABLE_NON_FINITE_MATH = asuto()  # '-cl-finite-math-only
     DISABLE_OPTIMISATIONS = auto()  # '-cl-opt-disable'
 
 
@@ -26,7 +26,8 @@ class Core:
     environment to develop in.
     """
 
-    def __init__(self, useGPU: bool = True) -> None:
+    def __init__(self, device = None, useGPU: bool = True) -> None:
+
         self._isUsingGPU = useGPU
         self._gl_interop = False
         self._clDevice = None
@@ -38,27 +39,35 @@ class Core:
         os.environ["AMPLXE_LOG_LEVEL"] = "TRACE"
         os.environ["TPSS_DEBUG"] = "1"
 
-        # Get the OpenCL Platforms
-        self._platform = cl.get_platforms()[0]
+        if device:
+            self._device = device
+            self._platform = device.platform
 
-        logging.debug('Initialising OpenCL Runtime - {:s}'.format(self.platform.name))
-
-        gpuDevices = self.gpuDevices  # get GPU devices of selected platform
-        cpuDevices = self.cpuDevices  # get GPU devices of selected platform
-
-        if len(gpuDevices) > 0 and useGPU:
-            self._device = gpuDevices[0]  # take first GPU
-            logging.debug('Using GPU - {:s} ({:s}) for OpenCL'.format(self.device.name, self.device.version))
-
-        elif len(cpuDevices) > 0:
-            self._device = cpuDevices[0]  # take first CPU
-            logging.debug('Using CPU for OpenCL')
         else:
-            raise RuntimeError('No OpenCL device currently available')
+            # Preselect the default platform and device
 
-        if useGPU and cl.have_gl() and self.hasGLShare():
+            # Get the OpenCL Platforms
+            self._platform = cl.get_platforms()[0]
+
+            logging.debug('Initialising OpenCL Runtime - {:s}'.format(self.platform.name))
+
+            gpuDevices = self.gpuDevices  # get GPU devices of selected platform
+            cpuDevices = self.cpuDevices  # get GPU devices of selected platform
+
+            if len(gpuDevices) > 0 and useGPU:
+                self._device = gpuDevices[0]  # take first GPU
+                logging.debug('Using GPU - {:s} ({:s}) for OpenCL'.format(self.device.name, self.device.version))
+
+            elif len(cpuDevices) > 0:
+                self._device = cpuDevices[0]  # take first CPU
+                logging.debug('Using CPU for OpenCL')
+            else:
+                raise RuntimeError('No OpenCL device currently available')
+
+        if self.isUsingGpu() and cl.have_gl() and self.hasGLShareExtension():
             from pyopencl.tools import get_gl_sharing_context_properties
 
+            # Try setting up an OpenCL context with a Gl_Sharing
             try:
                 self._context = cl.Context(
                         properties=[(cl.context_properties.PLATFORM,
@@ -73,7 +82,8 @@ class Core:
             self._context = cl.Context(properties=[(cl.context_properties.PLATFORM, self.platform)],
                                        devices=[self.device])
 
-    def enableCompilerCache(self, state : int) -> None:
+    @staticmethod
+    def enableCompilerCache(state : int) -> None:
         """
         Sets PyOpenCL to use compiler caching to improve warm-up time
 
@@ -81,13 +91,26 @@ class Core:
         """
         os.environ["PYOPENCL_NO_CACHE"] = '1' if (state == OpenCLFlags.ENABLE_CACHE) else '1'
 
-    def enableCompilerOutput(self, state: int) -> None:
+    @staticmethod
+    def enableCompilerOutput(state: int) -> None:
         """
         Sets PyOpenCL to enable the compiler output including errors
 
         :param state: provide a OpenCLFlag
         """
         os.environ["PYOPENCL_COMPILER_OUTPUT"] = '' if (state == OpenCLFlags. ENABLE_COMPILER_OUTPUT) else '1'
+
+    def isUsingGPU(self) -> bool:
+        """
+        Returns if the device is a GPU
+        """
+        return self.device.type == cl.device_type.GPU
+
+    def isUsingCPU(self) -> bool:
+        """
+        Returns if the device is a GPU
+        """
+        return self.device.type == cl.device_type.CPU
 
     @property
     def device(self) -> cl.Device:
@@ -117,9 +140,6 @@ class Core:
     def useGLInterop(self) -> bool:
         return self._gl_interop
 
-    def isUsingGPU(self) -> bool:
-        return self._isUsingGPU
-
     def hasSharedMemory(self) -> bool:
         """
         Returns if the currently selected Compute Device has shared memory capability (e.g. APU, Intel Platforms)
@@ -132,7 +152,7 @@ class Core:
             return False
 
     def deviceType(self) -> str:
-        if "Intel(R) UHD Graphics" in self.device.name:
+        if "Intel" in self.device.vendor and self.device.type == cl.device_type.GPU:
             return 'Intel GPU'
 
     ### Helper functions ###
@@ -164,6 +184,27 @@ class Core:
         :return: number of compute units available
         """
         return self.device.max_compute_units
+
+    @property
+    def maxWorkGroupSize(self) -> int:
+        """
+        The maximum work group size for the device
+        """
+        return self.device.max_work_group_size()
+
+    @property
+    def max2DImageSize(self) -> Tuple[int,int]:
+        """
+        The maximum 2D image size buffer
+        """
+        return self.device.image2d_max_width,  self.device.image2d_max_height
+
+    @property
+    def max3DImageSize(self) -> Tuple[int,int, int]:
+        """
+        The maximum 3D image size buffer
+        """
+        return self.device.image3d_max_width, self.device.image3d_max_height, self.device.image3d_max_depth
 
     @property
     def localMemorySize(self) -> int:
@@ -199,7 +240,7 @@ class Core:
          """
         return 'cl_khr_fp64' in self.platform.extensions
 
-    def hasGLShare(self) -> bool:
+    def hasGLShareExtension(self) -> bool:
         """
          Returns if the compute device has native GL Sharing Capabilities (within driver)
 
